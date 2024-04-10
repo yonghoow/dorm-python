c19a8b49ae53ebe5a99e150d2b70fa3f8116970f
yonghoow <2073951b@student.tp.edu.sg>
2024-04-11 06:07:51 +0800

    trying sort

--- read_door.py
+++ read_door.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
-df = pd.read_excel('../sample.xlsx')
+df = pd.read_excel('sample.xlsx', skiprows=1)
+
+#sorting by column 'UNIT NO'
+df.sort_values(by='UNIT NO', inplace=True)
 
 print(df)
+
+#reset index
+df.reset_index(drop=True, inplace=True)
+
+print('After resetting index:')
+print(df)
+
+#save to new excel file
+df.to_excel('sample_sorted.xlsx', index=False)
